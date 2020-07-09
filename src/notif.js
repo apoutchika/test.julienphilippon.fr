b0fb5b989b5ed7558c1998052c4f3d10b88b7cae
Julien Philippon <juphilippon@gmail.com>
2020-07-09 07:37:47 +0200

    Test

--- src/notif.js
+++ src/notif.js
@@ -1,33 +1,34 @@
 const Promise = require('bluebird')
 
-const OneSignal = window.OneSignal || []
+window.OneSignal = window.OneSignal || []
 
 export const setStatus = function (status) {
   console.log('coucou')
-  OneSignal.push(function () {
+  window.OneSignal.push(function () {
     console.log('1')
-    if (!OneSignal.isPushNotificationsSupported()) {
+    if (!window.OneSignal.isPushNotificationsSupported()) {
       console.log('push not supported')
       return false
     }
 
     console.log('Before promise')
     Promise.all([
-      OneSignal.isPushNotificationsEnabled(),
-      OneSignal.isOptedOut(),
+      window.OneSignal.isPushNotificationsEnabled(),
+      window.OneSignal.isOptedOut(),
     ]).then(function ([isPushEnabled, isOptedOut]) {
+      console.log([isPushEnabled, isOptedOut])
       if (isPushEnabled) {
         console.log('Subscribed, opt them out')
-        return OneSignal.setSubscription(false)
+        return window.OneSignal.setSubscription(false)
       } else {
         if (isOptedOut) {
           console.log('Opted out, opt them back in')
-          return OneSignal.setSubscription(true)
+          return window.OneSignal.setSubscription(true)
         } else {
           console.log('Unsubscribed, subscribe them')
-          return OneSignal.registerForPushNotifications().then((a) => {
+          return window.OneSignal.registerForPushNotifications().then((a) => {
             console.log('in then', a)
-            return OneSignal.setSubscription(true).then((b) => {
+            return window.OneSignal.setSubscription(true).then((b) => {
               console.log('in then', b)
             })
           })
